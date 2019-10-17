a5f8b0067af4d22a4963b7c7e12f0b8c4d88d30d
Manoj Singh <manojs@microsoft.com>
2019-10-16 23:57:21 -0400

    Updated

--- health_probe_handler.py
+++ health_probe_handler.py
@@ -1,9 +1,41 @@
-from bottle import Bottle, run, redirect
+# workserver.py - simple HTTP server with a do_work / stop_work API
+# GET /do_work activates a worker thread which uses CPU
+# GET /stop_work signals worker thread to stop
+import socket
+import time
 
-app = Bottle()
+from bottle import route, run
 
-@app.route('/hello')
-def hello():
-    return "Hello World!"
+hostname = socket.gethostname()
+hostport = 9000
+keepworking = False  # boolean to switch worker thread on or off
 
-run(app, host='localhost', port=8080)
+def writebody():
+    body = '<html><head><title>VM Health Check</title></head>'
+    body += '<body><h2> ' + hostname + ' is Healthy </h2><ul><h3>'
+    </body></html>'
+    return body
+
+
+@route('/')
+def root():
+    return writebody()
+
+
+@route('/do_work')
+def do_work():
+    global keepworking
+    # start worker thread
+    keepworking = True
+    return writebody()
+
+
+@route('/stop_work')
+def stop_work():
+    global keepworking
+    # stop worker thread
+    keepworking = False
+    return writebody()
+
+
+run(host=hostname, port=hostport)

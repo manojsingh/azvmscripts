fcb930fef9187970f7bd57ade5e8bb5f086b92cc
Manoj Singh <manojs@microsoft.com>
2019-11-08 14:35:22 -0500

    updated

--- install_dependencies_ubuntu.sh
+++ install_dependencies_ubuntu.sh
@@ -12,3 +12,9 @@ pip3 install configparser
 wget https://dl.influxdata.com/telegraf/releases/telegraf_1.8.0~rc1-1_amd64.deb 
 # install the package 
 sudo dpkg -i telegraf_1.8.0~rc1-1_amd64.deb
+
+# generate the new Telegraf config file in the current directory 
+telegraf --input-filter cpu:mem --output-filter azure_monitor config > azm-telegraf.conf 
+
+# replace the example config with the new generated config 
+sudo cp azm-telegraf.conf /etc/telegraf/telegraf.conf
